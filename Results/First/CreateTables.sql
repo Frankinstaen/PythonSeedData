93e24d0ff6ffc235479500cff1fde495bb131984
Frankinstaen <frankinstaen@mail.ru>
2023-04-17 19:05:29 +0300

    Поправил скрипт и резервную копию бд

--- Results/First/CreateTables.sql
+++ Results/First/CreateTables.sql
@@ -1,53 +1,112 @@
 use our_organization;
 
-create table departments(
-id int primary key identity not null,
-department VARCHAR(50) not null
-);
-
-create table pc (
-pc_id nvarchar(255) primary key not null,
-pc_serial NVARCHAR(25) not null,
-pc_mac NVARCHAR(40) not null,
-pc_ip nvarchar(100) not null
-);
-
-CREATE TABLE personal
+CREATE TABLE [dbo].[departments](
+	[id] [int] NOT NULL,
+	[department] [varchar](max) NULL,
+PRIMARY KEY CLUSTERED 
 (
-    user_id uniqueidentifier primary key not null,
-    first_name NVARCHAR(20) not null,
-    last_name NVARCHAR(20) not null,
-    birth_date date not null,
-    login VARCHAR(30) not null,
-	email VARCHAR(100) unique,
-	department_id int not null,
-	pc_id nvarchar(255) not null unique,
-	FOREIGN KEY (department_id) REFERENCES departments (id) ON DELETE CASCADE,
-	FOREIGN KEY (pc_id) REFERENCES pc (pc_id) ON DELETE CASCADE
-);
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
 
-create table login_dates (
-user_id uniqueidentifier not null,
-pc_id nvarchar(255) not null,
-date_time datetime2 not null,
-primary key (user_id, pc_id),
-FOREIGN KEY (user_id) REFERENCES personal (user_id) ON DELETE no action,
-FOREIGN KEY (pc_id) REFERENCES pc (pc_id) ON DELETE no action
-);
+CREATE TABLE [dbo].[pc](
+	[pc_id] [varchar](36) NOT NULL,
+	[pc_serial] [varchar](max) NULL,
+	[pc_mac] [varchar](max) NULL,
+	[pc_ip] [varchar](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[pc_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
 
-create table contracts (
-user_id uniqueidentifier primary key not null,
-date_from date not null,
-date_to date not null
-FOREIGN KEY (user_id) REFERENCES personal (user_id) ON DELETE CASCADE
-);
+CREATE TABLE [dbo].[personal](
+	[user_id] [varchar](36) NOT NULL,
+	[first_name] [varchar](max) NULL,
+	[last_name] [varchar](max) NULL,
+	[birth_date] [date] NULL,
+	[login] [varchar](max) NULL,
+	[email] [varchar](max) NULL,
+	[department_id] [int] NULL,
+	[pc_id] [varchar](36) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[user_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
 
-create table salary (
-user_id uniqueidentifier primary key not null,
-month int not null,
-year int not null,
-salary int not null,
-FOREIGN KEY (user_id) REFERENCES personal (user_id) ON DELETE CASCADE
-);
+ALTER TABLE [dbo].[personal]  WITH CHECK ADD FOREIGN KEY([department_id])
+REFERENCES [dbo].[departments] ([id])
+GO
 
-go
+ALTER TABLE [dbo].[personal]  WITH CHECK ADD FOREIGN KEY([pc_id])
+REFERENCES [dbo].[pc] ([pc_id])
+GO
+
+CREATE TABLE [dbo].[contracts](
+	[user_id] [varchar](36) NOT NULL,
+	[date_from] [date] NULL,
+	[date_to] [date] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[user_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[contracts]  WITH CHECK ADD FOREIGN KEY([user_id])
+REFERENCES [dbo].[personal] ([user_id])
+GO
+
+CREATE TABLE [dbo].[salary](
+	[salary_id] [int] IDENTITY(1,1) NOT NULL,
+	[user_id] [varchar](36) NULL,
+	[month] [int] NULL,
+	[year] [int] NULL,
+	[salary] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[salary_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+UNIQUE NONCLUSTERED 
+(
+	[user_id] ASC,
+	[month] ASC,
+	[year] ASC,
+	[salary] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[salary]  WITH CHECK ADD FOREIGN KEY([user_id])
+REFERENCES [dbo].[personal] ([user_id])
+GO
+
+CREATE TABLE [dbo].[login_dates](
+	[login_dates_id] [int] IDENTITY(1,1) NOT NULL,
+	[user_id] [varchar](36) NULL,
+	[pc_id] [varchar](36) NULL,
+	[date_time] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[login_dates_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+UNIQUE NONCLUSTERED 
+(
+	[user_id] ASC,
+	[pc_id] ASC,
+	[date_time] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[login_dates]  WITH CHECK ADD FOREIGN KEY([pc_id])
+REFERENCES [dbo].[pc] ([pc_id])
+GO
+
+ALTER TABLE [dbo].[login_dates]  WITH CHECK ADD FOREIGN KEY([user_id])
+REFERENCES [dbo].[personal] ([user_id])
+GO
